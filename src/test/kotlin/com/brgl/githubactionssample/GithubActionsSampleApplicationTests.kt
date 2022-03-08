e9ebd3485376baf8370351fc75431e9879a14313
Birgül Kütük <birgulkutukk@gmail.com>
2022-03-08 20:44:46 +0100

    Initial commit

---
+++ src/test/kotlin/com/brgl/githubactionssample/GithubActionsSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brgl.githubactionssample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubActionsSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
