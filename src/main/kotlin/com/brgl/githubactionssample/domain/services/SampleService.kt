9692292fe8bb9776666a5a8dfa4e6c0237309e86
Birgül Kütük <birgulkutukk@gmail.com>
2022-03-09 16:24:05 +0100

    5

--- src/main/kotlin/com/brgl/githubactionssample/domain/services/SampleService.kt
+++ src/main/kotlin/com/brgl/githubactionssample/domain/services/SampleService.kt
@@ -6,5 +6,5 @@ fun test_PR() {
 //    print("first commit on feature/v1 branch")
 //    print("second commit on feature/v1 branch")
 //    print("without PR merge directly")
-    print("without PR merge directly after add rule to settings")
+    print("without PR merge directly after add rule to settings 2")
 }
