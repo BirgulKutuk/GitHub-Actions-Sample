e50ec10a364fa8c261a61036100c92b98992b5dd
Birgül Kütük <birgulkutukk@gmail.com>
2022-03-09 13:03:05 +0100

    sample test

--- src/test/kotlin/com/brgl/githubactionssample/GithubActionsSampleApplicationTests.kt
+++ src/test/kotlin/com/brgl/githubactionssample/GithubActionsSampleApplicationTests.kt
@@ -1,13 +1,15 @@
 package com.brgl.githubactionssample
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class GithubActionsSampleApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun sampleTest() {
+        print("sample test 1")
+        Assertions.assertTrue(true)
+    }
 }
