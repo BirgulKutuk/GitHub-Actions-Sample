bf10f0f3bc573fc54838124212c9b82038fe676b
Birgül Kütük <birgulkutukk@gmail.com>
2022-05-03 22:59:04 +0200

    Update SampleService.kt

--- src/main/kotlin/com/brgl/githubactionssample/domain/services/SampleService.kt
+++ src/main/kotlin/com/brgl/githubactionssample/domain/services/SampleService.kt
@@ -7,5 +7,5 @@ fun test_PR() {
 //    print("second commit on feature/v1 branch")
 //    print("without PR merge directly")
 //    print("without PR merge directly after add rule to settings 2")
-    print("ktlint auto format")
+    print("ktlint auto format antrun")
 }
