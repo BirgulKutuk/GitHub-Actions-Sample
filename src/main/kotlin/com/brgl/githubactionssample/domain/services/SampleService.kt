7520538916b9f0cdf789a75fc3ac67a57c2bcbff
BRGL <runner@fv-az207-335.c40kwnxwbehe1hck2q0xzrihfd.cx.internal.cloudapp.net>
2022-05-04 13:14:25 +0000

    code automatically formatted with ktlint

--- src/main/kotlin/com/brgl/githubactionssample/domain/services/SampleService.kt
+++ src/main/kotlin/com/brgl/githubactionssample/domain/services/SampleService.kt
@@ -8,12 +8,8 @@ fun test_PR() {
 //    print("without PR merge directly")
 //    print("without PR merge directly after add rule to settings 2")
     print("ktlint auto format antrun")
-
 }
 
-
-fun test_2()
-{
+fun test_2() {
     print("test 2")
-    
 }
